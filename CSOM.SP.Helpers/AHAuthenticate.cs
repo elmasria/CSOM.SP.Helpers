09979e6d7f1d260b735fc94247bbbc831a578667
Ahmad EL Masri <aelmasri@iss-mea.com>
2018-12-12 09:59:41 +0400

    feat: add authorization

---
+++ CSOM.SP.Helpers/AHAuthenticate.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+
+namespace CSOM.SP.Helpers
+{
+    public class AHAuthenticate
+    {
+        public bool isUserAuthFor(string userName, string groupName, string sharePointURL = "Default")
+        {
+            string targetDomain = ConfigurationManager.AppSettings["Domain"];
+            string adminUserName = ConfigurationManager.AppSettings["SharepointUserName"];
+            string adminpassword = ConfigurationManager.AppSettings["SharepointPassword"];
+            sharePointURL = sharePointURL != "Default" ? sharePointURL : ConfigurationManager.AppSettings["SharepointURL"];
+
+            bool isAuth = false;
+            bool groupExists = false;
+            UserCollection collUser = null;
+            User user = null;
+            GroupCollection gcollection = null;
+
+            using (ClientContext context = new ClientContext(sharePointURL))
+            {
+                context.Credentials = new NetworkCredential(adminUserName, adminpassword, targetDomain);
+                Web site = context.Web;
+
+                try
+                {
+                    user = site.EnsureUser(userName);
+                    context.Load(user);
+                    context.ExecuteQuery();
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+
+                try
+                {
+                    gcollection = site.SiteGroups;
+                    context.Load(gcollection);
+                    context.ExecuteQuery();
+                }
+                catch (Exception)
+                {
+
+                    return false;
+                }
+
+                Group groupsa = gcollection.Where(gp => gp.Title == groupName).FirstOrDefault();
+
+
+                if (groupsa != null)
+                {
+                    // Group is matched with the required one
+                    // Fill collUser with users of the required group
+                    collUser = groupsa.Users;
+                    context.Load(collUser);
+                    context.ExecuteQuery();
+                    groupExists = true;
+
+                }
+                if (groupExists)
+                {
+                    User userin = collUser.Where(us => us.LoginName == user.LoginName).FirstOrDefault();
+
+                    if (userin != null)
+                    {
+                        // Target user exist
+                        // Giv user permission to edit
+                        isAuth = true;
+                    }
+                }
+                else
+                {
+                    isAuth = false;
+                }
+            }
+            return isAuth;
+        }
+    }
+}
---
+++ CSOM.SP.Helpers/AHNetwork.cs
@@ -0,0 +1,31 @@
+﻿using AHHelperTools;
+using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSOM.SP.Helpers
+{
+    public class AHNetwork
+    {
+        /// <summary>
+        /// To accept Form Based Authentication 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void ctx_MixedAuthRequest(object sender, WebRequestEventArgs e)
+        {
+            try
+            {
+                //Add the header that tells SharePoint to use Windows authentication.
+                e.WebRequestExecutor.RequestHeaders.Add("X-FORMS_BASED_AUTH_ACCEPTED", "f");
+            }
+            catch (Exception ex)
+            {
+                AHlogs.log(ex.Message, "csom-sp-helper");
+            }
+        }
+    }
+}
